81098fc87a7d657b333566ae0a1bb13c7ff41212
alazeprt <nono135246@126.com>
2024-10-21 17:32:36 +0800

    Fix the bug on merging

--- src/client/java/top/alazeprt/sls/mixin/client/ServerListMixin.java
+++ src/client/java/top/alazeprt/sls/mixin/client/ServerListMixin.java
@@ -2,6 +2,7 @@ package top.alazeprt.sls.mixin.client;
 
 import net.minecraft.client.network.ServerInfo;
 import net.minecraft.client.option.ServerList;
+import org.slf4j.Logger;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
